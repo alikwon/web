4f3cbb30fa113463f86be30b7f540313868152c0
alikwon <63032589+alikwon@users.noreply.github.com>
2020-06-15 18:20:54 +0900

    .

---
+++ html_JS/json_0615/json_test.js
@@ -0,0 +1,15 @@
+var obj = {
+    memberid:'cool',
+    memvername:'COOL',
+    age:27,
+    program:['java','python','javascript']
+}
+
+/* JSON 
+{
+    "memberid":"cool",
+    "memvername":"COOL",
+    "age":27,
+    "program":["java","python","javascript"]
+} 
+*/
